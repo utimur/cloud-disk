3d0415106164a4b406c2702c64ff507055d54932
utimur <akva_ten@mail.ru>
2020-06-21 16:56:28 +0300

    init app

---
+++ src/main/java/com/example/clouddisk/CloudDiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouddisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDiskApplication.class, args);
+	}
+
+}
